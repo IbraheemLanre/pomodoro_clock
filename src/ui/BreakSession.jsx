cf599ce4b6fb04fb65f99717b4190b43cfb4b17c
Ibraheem <talk2lanre205@yahoo.com>
2020-05-25 20:20:33 +0200

    updated

--- src/ui/BreakSession.jsx
+++ src/ui/BreakSession.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const BreakSessionContainer = ({children, ...props}) => {
     return(
-        <div className= "fle flex-col items-center" {...props}>
+        <div className= "flex flex-col items-center" {...props}>
             {children}
         </div>
     );
